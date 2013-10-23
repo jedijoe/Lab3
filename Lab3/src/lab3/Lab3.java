2a15a4ad71a87dc8b1dacdaa711b64da3321af6c
jedijoe <j_harris6@salemstate.edu>
2013-10-23 11:40:21 -0400

    Final merge

--- Lab3/src/lab3/Lab3.java
+++ Lab3/src/lab3/Lab3.java
@@ -1,30 +1,119 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab3;
-
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- *
- * @author s0257480
- */
 public class Lab3
 {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main( String[] args ) throws java.io.FileNotFoundException, java.io.IOException
-    {
-        BufferedReader reader = new BufferedReader(new FileReader("C:/matrices_values.txt"));
-        String line = null;
-        while ( ( line = reader.readLine() ) != null )
-        {
+   public static void main(String args[])
+   {
+      int a, b, x, y, i, i1, i2, i3, r1, r2;
+      double sum = 0 ;
 
-            System.out.println(line);
+      //First matrix ---------------------------------------------------//
+      String filename = "H:/Matrix1.txt"; // file that has the first matrix (update path)
+      System.out.print("Matrix1 = ");
+      a = readingMatrixSizeA(filename);
+      System.out.print(" by ");
+      b = readingMatrixSizeB(filename);
+      System.out.println();
+
+      double first[][] = new double[a][b];
+      for ( i = 0 ; i < a ; i++ )
+      {
+         for ( i1 = 0 ; i1 < b ; i1++ )
+         {   first[i][i1] = readingTheMatrix(filename,i,i1);  }
+      }
+      //----------------------------------------------------------------//
+
+
+      //Second Matrix---------------------------------------------------//
+      filename = "H:/Matrix2.txt"; // file that has the second matrix (update path)
+      System.out.print("Matrix2 = ");
+      x = readingMatrixSizeA(filename);
+      System.out.print(" by ");
+      y = readingMatrixSizeB(filename);
+      System.out.println();
+      if ( b != x )
+      {   System.out.println("Matrices multiplication is not possible.");   }
+      else
+      {
+
+      }
+   }
+
+   public static int readingMatrixSizeA( String thefile )
+    {
+    int a=0;
+    try {
+            // since we don't know the size of the matrix, store the values in an array list
+            // so that it will grow automatically as we read in the matrix
+            ArrayList< ArrayList< Double > > dynamicArray = new ArrayList< ArrayList< Double > >();
+
+            // connect a scanner to the file
+            Scanner fileReader = new Scanner(new File(thefile));
+
+            // while there is a row of values to parse
+            while(fileReader.hasNext())
+            {
+                // get the next line of the file
+                String input = fileReader.nextLine();
+                String[] values = input.split(" ");
+                ArrayList< Double > row = new ArrayList< Double >();
+
+                for(int index = 0; index < values.length; ++index)
+                {row.add(Double.parseDouble(values[index]));}
+                dynamicArray.add(row);
+            }
+            a = dynamicArray.size();
+
+            System.out.print(a);
         }
+    catch (FileNotFoundException e)
+        {
+            System.out.println("Failed to read in file: " + thefile + "!  Reason: " + e.getLocalizedMessage());
+        }
+    return a;
+    }
+
+   public static int readingMatrixSizeB( String thefile )
+    {
+    int b=0;
+    try {
+            // since we don't know the size of the matrix, store the values in an array list
+            // so that it will grow automatically as we read in the matrix
+            ArrayList< ArrayList< Double > > dynamicArray = new ArrayList< ArrayList< Double > >();
+
+            // connect a scanner to the file
+            Scanner fileReader = new Scanner(new File(thefile));
+
+            // while there is a row of values to parse
+            while(fileReader.hasNext())
+            {
+                // get the next line of the file
+                String input = fileReader.nextLine();
+                String[] values = input.split(" ");
+                ArrayList< Double > row = new ArrayList< Double >();
+                for(int index = 0; index < values.length; ++index)
+                {row.add(Double.parseDouble(values[index]));}
+                dynamicArray.add(row);
+            }
+            b = dynamicArray.get(0).size();
+            System.out.print(b);
+        }
+    catch (FileNotFoundException e)
+        {
+            System.out.println("Failed to read in file: " + thefile + "!  Reason: " + e.getLocalizedMessage());
+        }
+    return b;
+    }
+
+   public static double readingTheMatrix( String thefile, int a, int b)
+    {
+           
     }
 }
